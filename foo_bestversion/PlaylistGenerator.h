83f8a01597f8cf9225fc93c85798ef762abe9852
Ben Hymers <b.hymers@gmail.com>
2013-09-05 22:06:59 +0100

    Generate playlist on pressing OK button on text to playlist dialogue
    Overloads for generatePlaylistFromTracks to take tracks as vector
    Keep best versions around in dialogue after updating output text

--- foo_bestversion/PlaylistGenerator.h
+++ foo_bestversion/PlaylistGenerator.h
@@ -2,10 +2,14 @@
 
 #include "FoobarSDKWrapper.h"
 
+#include <vector>
+
 namespace bestversion {
 
 void generatePlaylistFromTracks(const pfc::list_t<metadb_handle_ptr>& tracks);
+void generatePlaylistFromTracks(const std::vector<metadb_handle_ptr>& tracks);
 void generatePlaylistFromTracks(const pfc::list_t<metadb_handle_ptr>& tracks, const std::string& playlistName);
+void generatePlaylistFromTracks(const std::vector<metadb_handle_ptr>& tracks, const std::string& playlistName);
 void replaceTrackInActivePlaylist(const metadb_handle_ptr& trackToReplace, const metadb_handle_ptr& replacement);
 
 } // namespace bestversion
